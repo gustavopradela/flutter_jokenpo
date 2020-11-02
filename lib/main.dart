12c2c268c391cdca5d230fbc5fea81745cee4dae
Gustavo pradela <gustavopradela@gmail.com>
2020-11-02 20:02:23 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:jokenpo/Jogo.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+
+
+
+
+  ));
+}
+
